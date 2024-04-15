dbf8dd54700a2af845de57785e18736116c9f53c
antonio neto <107404177+antxnio7@users.noreply.github.com>
2024-04-15 08:53:40 -0300

    sequiser

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ function verificarTime() {
 
   while (continuarPerguntando) {
     
-    var respostaTime = prompt("Qual o melhor time?");
+    var respostaTime = prompt("Qual o melhor professor da história?");
 
     
-    if (respostaTime.toLowerCase() === "athlético pr") {
-      alert("Isso mesmo! O Athlético PR é o melhor time!");
+    if (respostaTime.toLowerCase() === "ALMEIDA") {
+      alert("Isso mesmo! O Almeida é o melhor Professor!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
       alert("Não é isso! Tente novamente.");
